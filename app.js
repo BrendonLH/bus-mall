4161dd535a24ecf8162d506cb004c89dc21db371
Brendon Hampton <brendonhampton1988@gmail.com>
2019-11-26 23:55:57 -0800

    created and linked all files, moved images to img folder inside assignement, started on JS

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+console.log('proof of life'); 
+
+//global variables
+var picOne = document.getElementById('dish1');
+var picTwo = document.getElementById('dish2');
+var picArray = [];
+
+// display 2 images to the page 
+// image ta has 3 properties 'src' 'title' and 'alt'
+
+picOne.src = './img/bag.jpg';
+picOne.title = 'Bag';
+picOne.alt = 'Bag';
+
+picTwo.src = './img/dog-duck.jpg';
+picTwo.title = 'Dog Duck';
+picTwo.alt = 'Dog Duck';
+
+// make a constructor
+
+function image (src, name,) {
+    this.src = `../img/${src}.jpg`;
+    this.title = name;
+    this.alt = name;
+
+    picArray.push(this); 
+}
+new image ('bag', 'Bag');
+new image ('bag', 'Bag');
+new image ('bag', 'Bag');
+
+console.table(picArray);
