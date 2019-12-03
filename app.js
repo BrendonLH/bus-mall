8c9b835f385547130f8e00b107320938ef7e71b7
Brendon Hampton <brendonhampton1988@gmail.com>
2019-12-02 21:04:16 -0800

    added a new indexArray, started building the function to keep from duplicating

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 'use strict';
-console.log('proof of life'); 
+// console.log('proof of life'); 
 
 //global variables
-var picOne = document.getElementById('dish1');
-var picTwo = document.getElementById('dish2');
-var picThree = document.getElementById('dish3');
+var picOne = document.getElementById('picture1');
+var picTwo = document.getElementById('picture2');
+var picThree = document.getElementById('picture3');
 var picArray = [];
+var picIndex = [];
+
+function rerunImage() {
+    for( var i = 0; i < picIndex.length; i ++) {
+        while( picIndex[0] === picIndex[1]) {
+            picIndex = randomIndex(picArray.length);
+        }
+    }
+}
 
 // display 2 images to the page 
-// image ta has 3 properties 'src' 'title' and 'alt'
+// image tag has 3 properties 'src' 'title' and 'alt'
 
 // picOne.src = './img/bag.jpg';
 // picOne.title = 'Bag';
@@ -23,42 +32,52 @@ function image (src, name,) {
     this.src = `../img/${src}.jpg`;
     this.title = name;
     this.alt = name;
-
+    
     picArray.push(this); 
 }
 
 // create random helper function
+
 // random number function from MDN 
 function randomIndex(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
 function generateImages() {
-   var indexOne = randomIndex(picArray.length);
-   picOne.src = picArray[indexOne].src;
-   picOne.title = picArray[indexOne].title;
-   picOne.alt = picArray[indexOne].alt 
-
-   var indexTwo = randomIndex(picArray.length);
-
-   picTwo.src = picArray[indexTwo].src;
-   picTwo.title = picArray[indexTwo].title;
-   picTwo.alt = picArray[indexTwo].alt;
-
-   var indexThree = randomIndex(picArray.length);
-
-   
-   console.log(indexOne, indexTwo, indexThree);
+    var indexOne = randomIndex(picArray.length);
+    
+    picOne.src = picArray[indexOne].src;
+    picOne.title = picArray[indexOne].title;
+    picOne.alt = picArray[indexOne].alt 
+    
+    var indexTwo = randomIndex(picArray.length);
+    
+    
+    picTwo.src = picArray[indexTwo].src;
+    picTwo.title = picArray[indexTwo].title;
+    picTwo.alt = picArray[indexTwo].alt;
+    // picIndex.push(indexTwo);
+    
+    var indexThree = randomIndex(picArray.length);
+    
+    
+    picThree.src = picArray[indexThree].src
+    picThree.title = picArray[indexThree].title
+    picThree.alt = picArray[indexThree].alt
+    
+    // console.log(indexOne, indexTwo, indexThree);
+    picIndex.push(indexOne, indexTwo, indexThree);   
 }
 
 // create image on page function
 function createOnPage() {
-new image ('bag', 'Bag');
-new image ('bag', 'Bag');
-new image ('bag', 'Bag');
+    new image ('bag', 'Bag');
+    new image ('banana', 'Banana');
+    new image ('boots', 'Boots');
 }
 createOnPage();
 generateImages();
 
+console.log(picIndex);
 console.table(picArray);
 // console.log(Math.random());
