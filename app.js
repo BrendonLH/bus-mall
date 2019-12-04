828a18dc8ae200f534c36262fad691a03d94dee5
Brendon Hampton <brendonhampton1988@gmail.com>
2019-12-03 22:49:58 -0800

    made tje JS dryer for the Images

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var picThree = document.getElementById('picture3');
 var picArray = [];
 var picVote = document.getElementById('picVote');
 var voteRounds = 25;
+var pictureContainer = [picOne, picTwo, picThree];
 
 // make a constructor
 function image (src, name) {
@@ -32,6 +33,7 @@ function clickVote() {
             picArray[i].vote ++;
         }
     }
+    console.table(picArray);
     generateImages();
     console.table(picArray);
     if( voteRounds === 0) {
@@ -47,66 +49,78 @@ function randomIndex(max) {
 }
 
 function generateImages() {
-    var indexOne = randomIndex(picArray.length);
-
-    picOne.src = picArray[indexOne].src;
-    picOne.title = picArray[indexOne].title;
-    picOne.alt = picArray[indexOne].alt 
-    picArray[indexOne].viewed ++;
-    
-    var indexTwo = randomIndex(picArray.length);
-    while(indexTwo === indexOne) {
-        indexTwo = randomIndex(picArray.length);
+    var currentImages = [];
+    for(var i = 0; i < pictureContainer.length; i ++) {
+        var currentIndex = randomIndex(picArray.length);
+        while(currentImages.includes(currentIndex)) {
+            currentIndex = randomIndex(picArray.length);
+        }
+        currentImages.push(currentIndex);
+        pictureContainer[i].src = picArray[currentIndex].src;
+        pictureContainer[i].title = picArray[currentIndex].title;
+        pictureContainer[i].alt = picArray[currentIndex].alt 
+        picArray[currentIndex].viewed ++;
     }
-    
-    picTwo.src = picArray[indexTwo].src;
-    picTwo.title = picArray[indexTwo].title;
-    picTwo.alt = picArray[indexTwo].alt;
-    picArray[indexTwo].viewed ++;
-    // picIndex.push(indexTwo);
-    
-    var indexThree = randomIndex(picArray.length);
-    
-    while(indexThree === indexTwo || indexThree === indexOne) {
-        indexThree = randomIndex(picArray.length); 
-    }
-    
-    picThree.src = picArray[indexThree].src
-    picThree.title = picArray[indexThree].title
-    picThree.alt = picArray[indexThree].alt
-    picArray[indexThree].viewed ++;
-    console.log(indexOne, indexTwo, indexThree);
-    // picIndex.push(indexOne, indexTwo, indexThree);   
 }
 
-// create image on page function
-function createOnPage() {
-    new image ('bag', 'Bag');
-    new image ('banana', 'Banana');
-    new image ('boots', 'Boots');
-    new image ('bathroom', 'Bathroom');
-    new image ('boots', 'Boots');
-    new image ('breakfast', 'Breakfast');
-    new image ('bubblegum', 'BubbleGum');
-    new image ('chair', 'Chair');
-    new image ('cthulhu', 'Cthulhu');
-    new image ('dog-duck', 'Dog Duck');
-    new image ('dragon', 'Dragon');
-    new image ('pen', 'Pen');
-    new image ('pet-sweep', 'Pet Sweep');
-    new image ('scissors', 'Scissors');
-    new image ('shark', 'Shark');
-    new image ('sweep', 'Sweep');
-    new image ('tauntaun', 'TaunTaun');
-    new image ('unicorn', 'Unicorn');
-    new image ('usb', 'USB');
-    new image ('water-can', 'Water Can');
-    new image ('wine-glass', 'Wine Glass');
-}
-createOnPage();
-generateImages();
-picVote.addEventListener('click', clickVote)
-
-// console.log(picIndex);
-console.table(picArray);
-// console.log(Math.random());
+// picOne.src = picArray[indexOne].src;
+    // picOne.title = picArray[indexOne].title;
+    // picOne.alt = picArray[indexOne].alt 
+    // picArray[indexOne].viewed ++;
+    
+    // var indexTwo = randomIndex(picArray.length);
+    // while(indexTwo === indexOne) {
+        //     indexTwo = randomIndex(picArray.length);
+        // }
+        
+        // picTwo.src = picArray[indexTwo].src;
+    // picTwo.title = picArray[indexTwo].title;
+    // picTwo.alt = picArray[indexTwo].alt;
+    // picArray[indexTwo].viewed ++;
+    // // picIndex.push(indexTwo);
+    
+    // var indexThree = randomIndex(picArray.length);
+    
+    // while(indexThree === indexTwo || indexThree === indexOne) {
+        //     indexThree = randomIndex(picArray.length); 
+        // }
+        
+        // picThree.src = picArray[indexThree].src
+        // picThree.title = picArray[indexThree].title
+        // picThree.alt = picArray[indexThree].alt
+        // picArray[indexThree].viewed ++;
+        // console.log(indexOne, indexTwo, indexThree);
+        // picIndex.push(indexOne, indexTwo, indexThree);   
+        
+        
+        // create image on page function
+        function createOnPage() {
+            new image ('bag', 'Bag');
+            new image ('banana', 'Banana');
+            new image ('boots', 'Boots');
+            new image ('bathroom', 'Bathroom');
+            new image ('boots', 'Boots');
+            new image ('breakfast', 'Breakfast');
+            new image ('bubblegum', 'BubbleGum');
+            new image ('chair', 'Chair');
+            new image ('cthulhu', 'Cthulhu');
+            new image ('dog-duck', 'Dog Duck');
+            new image ('dragon', 'Dragon');
+            new image ('pen', 'Pen');
+            new image ('pet-sweep', 'Pet Sweep');
+            new image ('scissors', 'Scissors');
+            new image ('shark', 'Shark');
+            new image ('sweep', 'Sweep');
+            new image ('tauntaun', 'TaunTaun');
+            new image ('unicorn', 'Unicorn');
+            new image ('usb', 'USB');
+            new image ('water-can', 'Water Can');
+            new image ('wine-glass', 'Wine Glass');
+        }
+        createOnPage();
+        generateImages();
+        console.table(picArray);
+        picVote.addEventListener('click', clickVote)
+        
+        // console.log(picIndex);
+        // console.log(Math.random());
